d3d441a80d80c80cd852c1cfcd2d17e25fac563c
rokis1024 <rokas.kaulas.rublinskas@gmail.com>
2020-10-04 21:20:51 +0300

    Initial commit

---
+++ app/src/main/java/com/example/ball_in_a_maze/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.ball_in_a_maze;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
